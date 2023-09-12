fcf6a37dc67507a7fa4fc461606e65a7f1de8937
Andrew Kim <andrewkim09@hotmail.com>
2023-09-12 01:31:04 -0400

    version 3

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaPencilAlt } from 'react-icons/fa'
+import { FaPencilAlt, FaCheck } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { GetTasks } from './functions/GetTasks'
 
@@ -19,10 +19,15 @@ export const Tasks = () => {
           <div class = "flex flex-col items-center w-[100%] h-[100%]">
               {tasksList.map((data) => {
                 return(
-                  <div class = "flex flex-col items-center w-[80%] h-[100%] mt-14">
-                    <div>{data.title}</div>
-                    <div>{data.details}</div>
-                    <div>{data.date}</div>
+                  <div class = "flex w-[100%] w-[80%] mt-14 bg-[#f1f5f9] rounded-2xl shadow-lg h-[100%]">
+                    <div class = "flex flex-col w-[100%] h-[100%] p-2">
+                      <div class = " text-2xl font-medium">{data.title}</div>
+                      <div class = "h-[50%]">{data.details}</div>
+                      <div class = "text-right text-sm">Due: {data.date}</div>
+                    </div>
+                    <div>
+                      <button class = "rounded-2xl bg-green"><FaCheck/></button>
+                    </div>
                   </div>
                 )
               })}
